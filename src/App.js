ee48360902c274ca3cd7f7aaaaa5ce4ef3cc43e4
noam-moshe <noamoshe1234@gmail.com>
2022-06-06 08:52:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import{useEffect} from "react"
+import { BrowserRouter as Router ,Routes,Route} from 'react-router-dom';
 
+import './App.css';
+import Add from "./components/addBook";
+
+import BookList from "./components/bookList";
+import Edit from "./components/editBook";
+
+//יש בעיה בכל העדכון בלוקל סטורג. צריך לעדכן את הדטה כל פעם מחדש (יש לבדןק איך לעשות ביוזסטייט עם לוקל סטורג)
+//בעזה מחר אעבוד על כך
+//או שישר לעדכן לדאטהבייס אמיתי.
 function App() {
+  useEffect(()=>{
+const repository=[{id:"1",title:"aaa",author:"aaaa"},{id:"2",title:"bbb",author:"bbbb"}]
+  localStorage.setItem("repository",JSON.stringify(repository))
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <Router>
+      <Routes>
+      <Route path="/" element={ <BookList/>}/>
+      <Route path="/add" element={ <Add/>}/>
+      <Route path="/edit/:id" element={ <Edit/>}/>
+      </Routes>
+    
+      </Router>
   );
 }
 
